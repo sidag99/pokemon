93cdc8362bea2f6664bb20f38774e28b1ec1f193
siddharth <sidag991@gmail.com>
2023-05-30 11:31:27 +0530

    Initial dist subtree commit

--- src/api/pokemon-api.ts
+++
@@ -1,7 +0,0 @@
-import {Pokemon} from "@/models/Pokemon";
-
-export abstract class PokemonApi {
-    static async fetchPokemon(pokemonID: number): Promise<Pokemon> {
-        return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}/`).then(response => response.json()).then(data => data as Pokemon);
-    }
-}
--- src/service/pagination-manager.ts
+++
@@ -1,7 +0,0 @@
-export function paginationManager() {
-    function fetchPokemon() {
-
-    }
-
-    return {fetchPokemon}
-}
